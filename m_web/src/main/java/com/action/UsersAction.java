26c1c9b3db37d7396d96f4fb8db7b1c85ed46b3d
Your Name <email@example.com>
2018-02-04 16:47:56 +0800

    测试

--- m_web/src/main/java/com/action/UsersAction.java
+++ m_web/src/main/java/com/action/UsersAction.java
@@ -18,7 +18,7 @@ public class UsersAction {
     @RequestMapping("/findall")
     @ResponseBody
     public List<Users> findAll(){
-    	System.out.println("进入findall方法");
+    	System.out.println("进入findall方法！");
         UsersService us = new UsersServiceImpl();
         return us.findAll();
     }
